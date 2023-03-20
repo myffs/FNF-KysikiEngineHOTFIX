de040881aa568bebdcf432e2277fd2b834bb099e
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2023-03-20 11:07:51 -0400

    preparing for 0.2

--- source/CreditState.hx
+++ source/CreditState.hx
@@ -91,7 +91,12 @@ class CreditState extends MusicBeatState
 			['ninjamuffin99',		'ninjamuffin99',	"Programmer of Friday Night Funkin'",							'https://twitter.com/ninja_muffin99',	'CF2D2D'],
 			['PhantomArcade',		'phantomarcade',	"Animator of Friday Night Funkin'",								'https://twitter.com/PhantomArcade3K',	'FADC45'],
 			['evilsk8r',			'evilsk8r',			"Artist of Friday Night Funkin'",								'https://twitter.com/evilsk8r',			'5ABD4B'],
-			['kawaisprite',			'kawaisprite',		"Composer of Friday Night Funkin'",								'https://twitter.com/kawaisprite',		'378FC7']
+			['kawaisprite',			'kawaisprite',		"Composer of Friday Night Funkin'",								'https://twitter.com/kawaisprite',		'378FC7'],
+			[''],
+			['Shoutouts'],
+			['Discord',     'discord',      'For the rpcs and more',               '',          'FFFFFF'],
+			['ShadowMario',   'shadowmario',  'Psych engine creator \n( yes this engine is psych modified. )',           '',     'FFFFFF'],
+			[''],
 		];
 		
 		for(i in pisspoop){
