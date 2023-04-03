76cecc1728a1b86259c0a4d18585c48acd5c6f02
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2023-04-03 17:28:08 -0400

    wgalc

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -49,7 +49,7 @@ class OptionsState extends MusicBeatState
 			case 'Adjust Delay and Combo':
 				LoadingState.loadAndSwitchState(new options.NoteOffsetState());
 			case 'Page 2 -->':
-				LoadingState.loadAndSwitchState(new options.OptionsStatePage2());
+			       MusicBeatState.switchState(new OptionsStatePage2());
 		}
 	}
 
