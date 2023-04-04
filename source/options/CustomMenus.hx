b501c08585da934fae01388198b36adb92b8762c
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2023-04-04 18:41:09 -0400

    custom menus - shown or not?

---
+++ source/options/CustomMenus.hx
@@ -0,0 +1,71 @@
+package options;
+
+#if desktop
+import Discord.DiscordClient;
+#end
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.utils.Assets;
+import flixel.FlxSubState;
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.util.FlxSave;
+import haxe.Json;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
+import flixel.input.keyboard.FlxKey;
+import flixel.graphics.FlxGraphic;
+import Controls;
+
+using StringTools;
+
+class VisualsUISubState extends BaseOptionsMenu
+{
+	public function new()
+	{
+		title = 'Custom Menus';
+		rpcTitle = 'Custom Menus Settings'; //for Discord Rich Presence
+
+		var option:Option = new Option('Show Custom Menus',
+			"If Unchecked, This will hide your custom menus YOU made\n( Modding ).",
+			'idkSCM',
+			'bool',
+			true);
+		addOption(option);
+
+		super();
+	}
+
+	var changedMusic:Bool = false;
+	function onChangePauseMusic()
+	{
+		if(ClientPrefs.pauseMusic == 'None')
+			FlxG.sound.music.volume = 0;
+		else
+			FlxG.sound.playMusic(Paths.music(Paths.formatToSongPath(ClientPrefs.pauseMusic)));
+
+		changedMusic = true;
+	}
+
+	override function destroy()
+	{
+		if(changedMusic) FlxG.sound.playMusic(Paths.music('freakyMenu'));
+		super.destroy();
+	}
+
+	#if !mobile
+	function onChangeFPSCounter()
+	{
+		if(Main.fpsVar != null)
+			Main.fpsVar.visible = ClientPrefs.showFPS;
+	}
+	#end
+}
