2b471ab89c85b8c852c5f67495c35a23a7e6a148
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2023-04-04 16:18:30 -0400

    web shat

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -83,14 +83,14 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
 	public static var ratingStuff:Array<Dynamic> = [
-		['You Suck!', 0.2], //From 0% to 19%
-		['Shit', 0.4], //From 20% to 39%
-		['Bad', 0.5], //From 40% to 49%
-		['Bruh', 0.6], //From 50% to 59%
-		['Meh', 0.69], //From 60% to 68%
-		['Nice', 0.7], //69%
-		['Good', 0.8], //From 70% to 79%
-		['Great', 0.9], //From 80% to 89%
+		['Your Ass!', 0.2], //From 0% to 19%
+		['Ass', 0.4], //From 20% to 39%
+		['Wtf', 0.5], //From 40% to 49%
+		['Bleh', 0.6], //From 50% to 59%
+		['Decent', 0.69], //From 60% to 68%
+		['Average', 0.7], //69%
+		['Nice!', 0.8], //From 70% to 79%
+		['Amazing!', 0.9], //From 80% to 89%
 		['Sick!', 1], //From 90% to 99%
 		['Perfect!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
