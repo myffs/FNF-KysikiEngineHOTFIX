0bd190f3ecd661a85d0887762f6d29c7362df6ed
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2023-04-08 05:09:56 -0400

    Fix with the font type

--- source/CreditState.hx
+++ source/CreditState.hx
@@ -153,7 +153,7 @@ class CreditState extends MusicBeatState
 		add(descBox);
 
 		descText = new FlxText(50, FlxG.height + offsetThing - 25, 1180, "", 32);
-		descText.setFormat(Paths.font("muskeet.ttf"), 32, FlxColor.WHITE, CENTER/*, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK*/);
+		descText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER/*, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK*/);
 		descText.scrollFactor.set();
 		//descText.borderSize = 2.4;
 		descBox.sprTracker = descText;
