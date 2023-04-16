6d423b36b5f20fa9f6d50ce8e1db122a28de4ae8
MemeHovy <124418090+MemeHovy@users.noreply.github.com>
2023-04-16 18:31:50 -0400

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -111,7 +111,7 @@ class Main extends Sprite
 		dateNow = dateNow.replace(" ", "_");
 		dateNow = dateNow.replace(":", "'");
 
-		path = "./crash/" + "PsychEngine_" + dateNow + ".txt";
+		path = "./crash/" + "KysikiEngine_" + dateNow + ".txt";
 
 		for (stackItem in callStack)
 		{
