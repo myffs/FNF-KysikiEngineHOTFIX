19bd55778e7a05ef0db2832fc1e02f9a60f3d0ce
MemeHovy <124418090+MemeHovy@users.noreply.github.com>
2023-04-16 18:06:08 -0400

    Update Discord.hx

--- source/Discord.hx
+++ source/Discord.hx
@@ -1,5 +1,6 @@
 package;
 
+#if desktop
 import Sys.sleep;
 import discord_rpc.DiscordRpc;
 
@@ -17,7 +18,7 @@ class DiscordClient
 	{
 		trace("Discord Client starting...");
 		DiscordRpc.start({
-			clientID: "863222024192262205",
+			clientID: "1097281074784116811",
 			onReady: onReady,
 			onError: onError,
 			onDisconnected: onDisconnected
@@ -81,7 +82,7 @@ class DiscordClient
 		DiscordRpc.presence({
 			details: details,
 			state: state,
-			largeImageKey: 'icon',
+			largeImageKey: 'iconOG',
 			largeImageText: "Engine Version: " + MainMenuState.psychEngineVersion,
 			smallImageKey : smallImageKey,
 			// Obtained times are in milliseconds so they are divided so Discord can use it
@@ -100,3 +101,4 @@ class DiscordClient
 	}
 	#end
 }
+#end
